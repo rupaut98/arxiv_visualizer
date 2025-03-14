83ee93925d4fbf64215291fc1f1f512f52d59887
rupaut98 <rupakraut78@gmail.com>
2025-03-13 20:43:47 -0500

    Updated paper controllers api

--- app/controllers/api/v1/papers_controller.rb
+++ app/controllers/api/v1/papers_controller.rb
@@ -1,53 +1,47 @@
 class Api::V1::PapersController < ApplicationController
-    skip_before_action :authenticate_user, only: [:index, :show]
-  
-    def index
-      if params[:query].present?
-        papers = ArxivService.search(params[:query], params[:start] || 0, params[:max_results] || 10)
-        render json: papers
-      else
-        render json: [], status: :ok
-      end
-    end
-  
-    def show
-      paper = Paper.find_by(arxiv_id: params[:id]) || find_and_create_paper(params[:id])
+  skip_before_action :authenticate_user, only: [:index, :show]
+
+  def index
+    if params[:query].present?
+      # Construct ArXiv API URL with query parameters
+      base_url = "http://export.arxiv.org/api/query"
+      query_params = {
+        search_query: params[:query],
+        start: params[:start] || 0,
+        max_results: params[:max_results] || 10,
+        sortBy: params[:sort_by] || 'relevance'
+      }
       
-      if paper
-        render json: paper.as_json(include: :citations)
-      else
-        render json: { error: 'Paper not found' }, status: :not_found
-      end
-    end
-  
-    def citations
-      paper = Paper.find_by(arxiv_id: params[:id])
+      response = HTTParty.get(base_url, query: query_params)
+      papers = ArxivService.parse_response(response.body)
       
-      if paper
-        citations = paper.citations.map do |citation|
-          cited_paper = Paper.find_by(arxiv_id: citation.cited_paper_arxiv_id)
-          if cited_paper
-            cited_paper.as_json
-          else
-            { arxiv_id: citation.cited_paper_arxiv_id }
-          end
-        end
-        
-        render json: citations
-      else
-        render json: { error: 'Paper not found' }, status: :not_found
-      end
-    end
-  
-    private
-  
-    def find_and_create_paper(arxiv_id)
-      paper_data = ArxivService.get_paper(arxiv_id)
-      return nil unless paper_data
-      
-      paper = Paper.create(paper_data)
-      
-      paper
+      render json: papers
+    else
+      render json: [], status: :ok
     end
   end
-  
+
+  def show
+    paper = Paper.find_by(arxiv_id: params[:id])
+    
+    unless paper
+      # If not in database, fetch from ArXiv
+      arxiv_paper = ArxivService.get_paper(params[:id])
+      
+      if arxiv_paper
+        paper = Paper.create(
+          arxiv_id: arxiv_paper[:arxiv_id],
+          title: arxiv_paper[:title],
+          authors: arxiv_paper[:authors],
+          abstract: arxiv_paper[:abstract],
+          url: arxiv_paper[:url],
+          published_date: arxiv_paper[:published_date]
+        )
+      else
+        return render json: { error: 'Paper not found' }, status: :not_found
+      end
+    end
+    
+    render json: paper.as_json
+  end
+end
