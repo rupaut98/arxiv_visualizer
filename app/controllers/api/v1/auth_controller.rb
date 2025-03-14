8adb07d6f4b8bcff0a0194758f8412a8a538dcf8
rupaut98 <rupakraut78@gmail.com>
2025-03-13 20:02:05 -0500

    Added authentication

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::AuthController < ApplicationController
+    skip_before_action :authenticate_user, only: [:login, :register]
+  
+    def register
+      user = User.new(user_params)
+      
+      if user.save
+        token = generate_token(user.id)
+        render json: { user: user.as_json(except: :password_digest), token: token }, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def login
+      user = User.find_by(email: params[:email])
+      
+      if user && user.authenticate(params[:password])
+        token = generate_token(user.id)
+        render json: { user: user.as_json(except: :password_digest), token: token }
+      else
+        render json: { error: 'Invalid email or password' }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:email, :password, :username)
+    end
+  
+    def generate_token(user_id)
+      JWT.encode({ user_id: user_id }, Rails.application.secrets.secret_key_base)
+    end
+  end
+  
